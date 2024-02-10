2cde1c303dd56a4a35c4808ea3ed1084c5efddc4
Mateus1906 <114652380+Mateus1906@users.noreply.github.com>
2024-02-10 19:07:51 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,24 @@
 provider "azurerm" {
-  features {}
+  version = "~>2.0"
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  description = "Azure service principal client ID"
+}
+
+variable "client_secret" {
+  description = "Azure service principal client secret"
+}
+
+variable "tenant_id" {
+  description = "Azure Active Directory tenant ID"
 }
